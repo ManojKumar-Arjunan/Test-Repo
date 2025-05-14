ca76d64f635c004bf5208994ab634e03940f16b4
Manoj Kumar Arjunan <manoj.k@applab.qa>
2025-05-14 17:25:03 +0530

    Initial Commit

---
+++ PodCommandStudy/PodCommandStudy/PodCommandStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodCommandStudyApp.swift
+//  PodCommandStudy
+//
+//  Created by Manoj Kumar Arjunan on 14/05/25.
+//
+
+import SwiftUI
+
+@main
+struct PodCommandStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
